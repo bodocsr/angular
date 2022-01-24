56f3c61b3a89ac9dd8ece17ae0d9a3e1acd777fe
bodocsr <bodocsr@gmail.com>
2022-01-24 09:40:31 +0100

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+//import { runInThisContext } from 'vm';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Calculator';
+  input: string = '';
+  answered: boolean = false;
+
+
+  onDelete() {
+    if (this.input.length > 0) {
+      this.input = this.input.substr(0, this.input.length - 1)
+    }
+  }
+  onReset(){
+    this.input = '';
+  }
+
+  pressNum(num: string) {
+    if (this.answered === true){
+      this.input = num;
+      this.answered = false;
+    }else{
+      this.input = this.input + num
+    }
+  }
+
+  pressOperator(op: string) {
+    this.answered = false;
+    if ((this.input === "" || (this.input ==="+" && this.input.length === 1)) && op ==='+') {
+      return
+    }
+    const lastKey = this.input[this.input.length - 1];
+    if (lastKey === '-' || lastKey === '+') {
+      this.input=this.input.replace(lastKey,op);
+      return;
+    }
+    this.input = this.input + op;
+  }
+
+
+  calcAnswer(){
+    this.input=eval(this.input);
+    this.input=this.input.toString();
+    this.answered = true;
+  }
 }
+
